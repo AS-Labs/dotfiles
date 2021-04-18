c04063c1fd13b1cccfb22d555328c5542601dede
as-labs <46945604+AS-Labs@users.noreply.github.com>
2021-04-18 10:03:28 +0300

    Create gitcl.sh

---
+++ gitcl.sh
@@ -0,0 +1,17 @@
+# To clone my repo
+gitArray=("git@github.com:AS-Labs/dont_sleep_lock.git" "git@github.com:AS-Labs/myWeb-3aqar.git" "git@gith
+ub.com:AS-Labs/dotfiles.git" "git@github.com:AS-Labs/BotterOtter.git" "git@github.com:AS-Labs/qtile.git" 
+"git@github.com:AS-Labs/learning-tutorials.git" "git@github.com:AS-Labs/c_code_course.git" "git@github.co
+m:AS-Labs/download-knet.git" "git@github.com:AS-Labs/web3aqar.git" "git@github.com:AS-Labs/3aqar.git" "gi
+t@github.com:AS-Labs/UN_AUTO.git" "git@github.com:AS-Labs/web-monitoring-dashboard.git" "git@github.com:A
+S-Labs/imperial_books.git" "git@github.com:AS-Labs/czipfile_py3.git" "git@github.com:AS-Labs/Proj_Webscra
+per.git" "git@github.com:AS-Labs/Countdown-react-r.git" "git@github.com:AS-Labs/Py-loop.git" "git@github.
+com:AS-Labs/hello-world.git")
+
+echo " Cloning repos for AS-Labs"
+for i in "${gitArray[@]}"
+do 
+	echo "starting clone for $i "
+	git clone $i
+done
+echo " Job completed "

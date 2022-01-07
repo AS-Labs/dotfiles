f33e5a579c02e2ea0d9d3a684c188a10a2e1bf42
as-labs <a.alshaiji@protonmail.com>
2022-01-07 11:46:09 +0300

    Added config creation

--- promtail_install_linux.sh
+++ promtail_install_linux.sh
@@ -5,6 +5,29 @@ chmod a+x promtail-linux-amd64
 sudo mv promtail-linux-amd64 /usr/bin/promtail
 sudo useradd -rs /bin/false promtail
 
+echo " Creating config file "
+sudo tee -a /usr/local/bin/config-promtail.yml <<EOF
+server:
+  http_listen_port: 9080
+  grpc_listen_port: 0
+
+positions:
+  filename: /tmp/positions.yaml
+
+clients:
+  - url: http://192.168.0.154:3100/loki/api/v1/push
+
+scrape_configs:
+- job_name: system
+  static_configs:
+  - targets:
+      - localhost
+    labels:
+      job: varlogs
+      __path__: /var/log/*/*log
+
+EOF
+
 sudo echo "[Unit]" >> /etc/systemd/system/promtail.service
 sudo echo "Description=Promtail" >> /etc/systemd/system/promtail.service
 sudo echo "After=network.target" >> /etc/systemd/system/promtail.service
@@ -13,7 +36,8 @@ sudo echo "[Service]" >> /etc/systemd/system/promtail.service
 sudo echo "User=promtail" >> /etc/systemd/system/promtail.service
 sudo echo "Group=promtail" >> /etc/systemd/system/promtail.service
 sudo echo "Type=simple" >> /etc/systemd/system/promtail.service
-sudo echo "ExecStart=/usr/bin/promtail" >> /etc/systemd/system/promtail.service
+sudo echo "ExecStart=/usr/bin/promtail -config.file /usr/local/bin/config-promtail.yml
+" >> /etc/systemd/system/promtail.service
 sudo echo "" >> /etc/systemd/system/promtail.service
 sudo echo "[Install]" >> /etc/systemd/system/promtail.service
 sudo echo "WantedBy=multi-user.target" >> /etc/systemd/system/promtail.service
